91a3654c5fb006dc41bf0db18aac60e82c088f46
rafiqulislam276 <70791224+rafiqulislam276@users.noreply.github.com>
2020-09-05 11:30:09 +0600

    Add files via upload

---
+++ Copy_string_using_strcpy_funcation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char source[]="Toufiqul Islam Tanmoy";
+    char terget[20];
+    strcpy( terget,source);
+    printf("Source: %s",terget);
+    printf("\nTarget source: %s",terget);
+
+
+
+
+
+}
+
+
